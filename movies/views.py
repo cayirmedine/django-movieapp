861f51980968898ab5586387523f3daa095c3f51
cayirmedine <htc.mdn.2010@gmail.com>
2024-06-17 18:23:07 +0300

    Some basic views defined for movies app

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Index")
+
+def movies(request):
+    return HttpResponse("Movies")
+
+def movie_details(request, slug):
+    return HttpResponse("Movie Detail: " + slug)
